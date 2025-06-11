911bc7e6a05f9dc5223cdae8f7c26e56d5732754
Jessica Loris <dearjess535@gmail.com>
2025-06-11 11:36:59 -0300

    calcular média do aluno

--- Aula01/src/Aula01/Exercicio08.java
+++ Aula01/src/Aula01/Exercicio08.java
@@ -1,5 +1,39 @@
 package Aula01;
 
-public class Exercicio08 {
+import java.util.Scanner;
 
+public class Exercicio08 {
+	public static void main(String[] args) {
+		Scanner  input = new Scanner(System.in);
+		System.out.println("Digite um número: ");
+		int data = input.nextInt();
+		
+		switch (data) {
+		case 1: 
+			System.out.println("Domingo!");
+			break;
+		case 2:
+			System.out.println("Segunda-feira!");
+			break;
+		case 3:
+			System.out.println("Terça-feira!");
+			break;
+		case 4:
+			System.out.println("Quarta-feira!");
+			break;
+		case 5:
+			System.out.println("Quinta-feira!");
+			break;
+		case 6:
+			System.out.println("Sexta-feira!");
+			break;
+		case 7:
+			System.out.println("Sábado!");
+			break;
+			
+		default:
+			System.out.println("Valor inválido!!");
+		}	
+	}
 }
+
---
+++ Aula01/src/Aula01/Exercicio09.java
@@ -0,0 +1,21 @@
+package Aula01;
+
+import java.util.Scanner;
+
+public class Exercicio09 {
+ public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+	
+	double nota01, nota02, media;
+	
+	System.out.println("Digite a primeira nota: ");
+	nota01 = input.nextDouble();
+	
+	System.out.println("Digite a segunda nota nota: ");
+	nota02 = input.nextDouble();
+	
+	media = nota01 + nota02 / 2;
+	
+	System.out.printf ("A média do aluno é: %.2f", media);
+ 	}
+}
