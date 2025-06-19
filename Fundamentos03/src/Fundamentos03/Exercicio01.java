8fee4a188a1fcf8477f682023219d3f372d60e3a
Jessica Loris <dearjess535@gmail.com>
2025-06-19 09:56:37 -0300

    arrays

---
+++ Fundamentos03/src/Fundamentos03/Exercicio01.java
@@ -0,0 +1,19 @@
+package Fundamentos03;
+
+public class Exercicio01 {
+ public static void main(String[] args) {
+	String str = new String();
+	System.out.println(str);
+	
+	
+	String s1 = "Hello";
+	String s2 = "HELLO";
+	boolean b1 = s1.equals("Hello");
+	boolean b2 = s1.equals(s2);
+	boolean b3 = s1.equalsIgnoreCase(s2);
+	boolean b4 = s1.equalsIgnoreCase("azul");
+	
+	
+	
+}
+}
